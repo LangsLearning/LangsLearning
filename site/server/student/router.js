9d5409c8c603d8d277f2d7cc7145ab1ff9f22a4a
Lara Natale Felício <lara.natalefelicio@gmail.com>
2020-12-04 17:38:13 -0300

    added deals page

--- site/server/student/router.js
+++ site/server/student/router.js
@@ -1,9 +1,11 @@
 
 module.exports = {
     apply: (mongoClient, app) => {
-        app.get('/student', (req, res) => {
-            console.log(req);
-            res.render('student');
+        app.get('/student/bookaclass', (req, res) => {
+            res.render('student_bookclass');
+        });
+        app.get('/student/packages', (req, res) => {
+            res.render('student_packages');
         });
     }
 };
