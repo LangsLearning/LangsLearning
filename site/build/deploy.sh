0838e6906696e1955e3d06074e5ca6494ba462ee
Leonardo Silva <leonardo.silva@mobimeo.com>
2020-12-15 20:22:24 +0100

    cleanup and deploy

---
+++ site/build/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+current_date=$(date +'%d_%m_%Y')
+file_name="site_${current_date}.zip"
+zip -q -r "${file_name}" Procfile package* server/ src/
+aws elasticbeanstalk delete-application-version --application-name "langslearning-site"
+aws s3 cp "${file_name}" "s3://elasticbeanstalk-sa-east-1-870767750120/$file_name"
+aws elasticbeanstalk create-application-version --application-name "langslearning-site" --version-label "${current_date}" --source-bundle S3Bucket="elasticbeanstalk-sa-east-1-870767750120",S3Key="${file_name}"
+aws elasticbeanstalk update-environment --environment-name "LangslearningSite-env" --version-label "${current_date}"
