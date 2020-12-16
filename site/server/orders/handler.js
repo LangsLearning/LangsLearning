9e2aa01ef8f2147527e76096b9b45d60484b0200
Leonardo Silva <leonardo.silva@mobimeo.com>
2020-12-16 19:27:42 +0100

    fix order classes

--- site/server/orders/handler.js
+++ site/server/orders/handler.js
@@ -1,4 +1,5 @@
 const md5 = require('md5');
+const sha1 = require('sha1');
 const pino = require("pino");
 const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
 
@@ -6,9 +7,7 @@ const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
 const apiKeys = ['ccb291bc22'];
 
 const isValidSid = (order) => {
-    const preSid = md5(Object.keys(order).reduce((acc, curr) => acc + curr));
-    const validSids = apiKeys.map(apiKey => preSid + apiKey);    
-    return validSids.includes(order.sid);
+    return order.nsid === sha1(order.edz_fat_cod + order.edz_cnt_cod + order.edz_cli_cod);
 };
 
 const products = [{
@@ -20,11 +19,25 @@ const registerOrder = studentRepository => (req, res) => {
     const order = req.body;
     logger.info(`New order received: ${JSON.stringify(order)}`);
     if (isValidSid(order)) {
-        const student = {name:'', }
-        studentRepository.registerOrUpdate(order)
-        res.status(200).json({});
+        const email = order.edz_cli_email;
+        const classesToAdd = products.find(product => product.id === order.edz_cnt_cod).classes;
+        if (!classesToAdd) {
+            logger.error(`Invalid product received: ${order.edz_cnt_cod} for customer ${email}`);
+            res.status(400).json({ message: "Invalid product" });
+            return;
+        }
+
+        studentRepository.addAvailableClasses(email, classesToAdd)
+            .then(student => {
+                res.status(200).json({});
+            })
+            .catch(err => {
+                logger.error(`Not possible to add new available classes to the student, ${err}`);
+                res.status(500).json({ message: err });
+            });
     } else {
-        res.status(401).json({message: "Not authorized"});
+        logger.error(`Invalid order request, invalid SID`);
+        res.status(401).json({ message: "Not authorized" });
     }
 };
 
