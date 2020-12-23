b4f958989681df9320f5f150fd9f32a8e4900075
Leonardo Silva <leonardo.silva@mobimeo.com>
2020-12-23 17:29:59 +0100

    move trials pages

--- site/server/trial/router.js
+++ site/server/trial/router.js
@@ -4,9 +4,9 @@ module.exports = {
     apply: (mongoClient, app) => {
         const handler = require('./handler')(mongoClient);
 
-        app.get('/trials', passport.authenticate('basic'), handler.getTrials);
-        app.post('/trials', passport.authenticate('basic'), handler.registerTrial);
-        app.post('/trials/level', passport.authenticate('basic'), handler.setLevel);
-        app.get('/trials/:id/remove', passport.authenticate('basic'), handler.removeTrial);
+        app.get('/admin/trials', passport.authenticate('basic'), handler.getTrials);
+        app.post('/admin/trials', passport.authenticate('basic'), handler.registerTrial);
+        app.post('/admin/trials/level', passport.authenticate('basic'), handler.setLevel);
+        app.get('/admin/trials/:id/remove', passport.authenticate('basic'), handler.removeTrial);
     }
 }
