c7362aaf57de2b867e8b5170cea5f7e00c4240f1
Leonardo Silva <leosilvadev@gmail.com>
2021-05-12 20:08:53 +0200

    refactor class

--- server/class/handler.js
+++ server/class/handler.js
@@ -2,92 +2,72 @@ const logger = require('../logger'),
     mail = require('../contact/mail'),
     ejs = require('ejs'),
     path = require('path'),
+    _ = require('lodash'),
     { serverConfig } = require('../config'),
     moment = require('moment'),
     Class = require('./class'),
-    { Teacher } = require('../teacher');
+    { Teacher } = require('../teacher'),
+    Handler = require('../handler');
 
-const getClasses = (req, res) => Class.find({})
-    .then(classes =>
-        Teacher.find({ active: true }).map(teachers => ({ classes, teachers }))
-    )
-    .then(data => {
-        const { classes, teachers } = data;
-        const updatedClasses = classes.map(aClass => {
-            aClass.teacher = teachers.find(teacher => aClass.teacherId == teacher._id);
-            return aClass;
-        });
-        return { classes: updatedClasses, teachers };
-    })
-    .then(data => {
-        const { classes, teachers } = data;
-        logger.info(`Rendering admin classes page: ${classes.length} classes found, ${teachers.length} teachers available`);
-        res.render('admin_classes', { classes, teachers, moment });
-    })
-    .catch(err => {
-        logger.error(err);
-        res.render('admin_classes', { classes: [], teachers: [], error: err.message, moment });
+
+const getClasses = new Handler(async(req, res) => {
+    const classes = await Class.find({});
+    const teachers = await Teacher.find({ active: true });
+
+    const classesWithTeacher = classes.map(async(aClass) => {
+        const teacher = await teachers.find(teacher => aClass.teacherId == teacher._id);
+        return { teacher, ...aClass };
     });
 
-const registerClass = (req, res) => {
-    const newClass = { title, description, level, datetime, materialLink, classLink } = req.body;
+    logger.info(`Rendering admin classes page: ${classesWithTeacher.length} classes found, ${teachers.length} teachers available`);
+    res.render('admin_classes', { classes, teachers, moment });
+
+}).onErrorRender('admin_classes', err => ({ classes: [], teachers: [], error: err.message, moment }));
+
+const registerClass = new Handler(async(req, res) => {
+    const aClass = { title, description, level, datetime, materialLink, classLink } = req.body;
     if (!title || !description || !level || !datetime || !materialLink || !classLink) {
         logger.error(`Invalid class data to be registered, ${JSON.stringify(req.body)}`);
         res.redirect('/admin/classes');
         return;
     }
 
-    Class.register(newClass)
-        .then(registered => {
-            logger.info(`Class ${JSON.stringify(registered)} registered`);
-            res.redirect('/admin/classes');
-        })
-        .catch(err => {
-            logger.error(err);
-            res.redirect('/admin/classes');
-        });
-};
+    const registered = await Class.register(aClass);
+    logger.info(`Class ${JSON.stringify(registered)} registered`);
+    res.redirect('/admin/classes');
 
-const removeClass = (req, res) => {
+}).onErrorRedirect('/admin/classes');
+
+const removeClass = new Handler(async(req, res) => {
     const { id } = req.params;
-    Class.deactivateById(id)
-        .then(result => {
-            res.redirect('/admin/classes');
-        })
-        .catch(err => {
-            logger.error(err);
-            res.redirect('/admin/classes');
-        });
-};
+    const _ = await Class.deactivateById(id);
+    res.redirect('/admin/classes');
 
-const assignTeacher = (req, res) => {
+}).onErrorRedirect('/admin/classes');
+
+const assignTeacher = new Handler(async(req, res) => {
     const { classId, teacherId } = req.body;
     if (!classId || !teacherId) {
         res.redirect('/admin/classes');
         return;
     }
 
-    Class.assignTeacher(classId, teacherId)
-        .then(result => {
-            res.redirect('/admin/classes');
-        })
-        .catch(err => {
-            logger.error(err);
-            res.redirect('/admin/classes');
-        });
-};
+    const _ = await Class.assignTeacher(classId, teacherId);
+    res.redirect('/admin/classes');
 
-const opsDumpAll = (req, res) => {
-    Class.deleteMany({})
-        .then(result => res.status(200).json({ message: 'All classes deleted' }))
-        .catch(err => res.status(500).json({ message: err }));
-};
+}).onErrorRedirect('/admin/classes');
 
-const opsFindAll = (req, res) => {
-    Class.find({})
-        .then(trials => res.status(200).json(trials))
-        .catch(err => res.status(500).json({ message: err }));
-};
+const opsDumpAll = new Handler(async(req, res) => {
+    const _ = await Class.deleteMany({});
+    res.status(200).json({ message: 'All classes deleted' });
+
+}).onErrorReturnJson(500, err => ({ message: err }));
+
+const opsFindAll = new Handler(async(req, res) => {
+    const classes = await Class.find({});
+    res.status(200).json(classes);
+
+}).onErrorReturnJson(500, err => ({ message: err }));
 
 module.exports = {
     getClasses,
---
+++ server/handler.js
@@ -0,0 +1,36 @@
+const logger = require('./logger');
+
+class Handler {
+    constructor(fn) {
+        this.fn = fn;
+    }
+
+    onErrorRedirect(path) {
+        return this.wrapFn((res, err) => {
+            res.redirect(path);
+        });
+    }
+
+    onErrorRender(path, buildParamsFn = err => ({})) {
+        return this.wrapFn((res, err) => {
+            res.render(path, buildParamsFn(err));
+        });
+    }
+
+    onErrorReturnJson(status, buildJsonFn = err => ({})) {
+        return this.wrapFn((res, err) => {
+            res.status(status)(path, buildJsonFn(err));
+        });
+    }
+
+    wrapFn(onErrorFn) {
+        return (req, res) => {
+            this.fn(req, res).catch(err => {
+                logger.error(err);
+                onErrorFn(res, err);
+            });
+        }
+    }
+}
+
+module.exports = Handler;
