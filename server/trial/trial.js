b69b8e5b380c634dd997b9bb42364dcc4a6d1794
Leonardo Silva <leosilvadev@gmail.com>
2021-05-12 17:10:08 +0200

    cleanup

---
+++ server/trial/trial.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose'),
+    logger = require('../logger');
+
+const TrialSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    datetime: Date,
+    link: String,
+    level: String
+});
+
+TrialSchema.statics.register = function(object) {
+    const trial = new Trial(object);
+    logger.info(`Registering Trial ${trial}`);
+    return trial.save();
+};
+
+TrialSchema.statics.setLevel = function(object) {
+    logger.info(`Set level for Trial with id ${id} to level ${level}`);
+    return Trial.updateOne({ _id: id }, { $set: { level } });
+};
+
+module.exports = mongoose.model('Trial', TrialSchema);
