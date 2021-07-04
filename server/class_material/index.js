d2d6a6468ceeca40bea2f97f3578f85632c6c044
Leonardo Silva <leosilvadev@gmail.com>
2021-07-04 09:51:03 +0200

    register class template

---
+++ server/class_material/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    ClassMaterialRepository: require('./repository'),
+};
--- server/index.js
+++ server/index.js
@@ -1,34 +1,38 @@
 const express = require('express'),
-cookieParser = require('cookie-parser'),
-session = require('express-session'),
-MongoStore = require('connect-mongo')(session),
-bodyParser = require('body-parser'),
-expressPino = require('express-pino-logger'),
-logger = require('./logger'),
-expressLogger = expressPino({ logger }),
-passport = require('passport'),
-cors = require('cors'),
-mail = require('./contact/mail'),
-mongoose = require('mongoose'),
-port = process.env.PORT || 3000;
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    MongoStore = require('connect-mongo')(session),
+    bodyParser = require('body-parser'),
+    expressPino = require('express-pino-logger'),
+    logger = require('./logger'),
+    expressLogger = expressPino({ logger }),
+    passport = require('passport'),
+    cors = require('cors'),
+    mail = require('./contact/mail'),
+    mongoose = require('mongoose'),
+    fileUpload = require('express-fileupload'),
+    port = process.env.PORT || 3000;
 
 const uri =
-"mongodb+srv://langslearning:q1w2e3@cluster0.zuiev.mongodb.net/langslearning?retryWrites=true&w=majority";
+    "mongodb+srv://langslearning:q1w2e3@cluster0.zuiev.mongodb.net/langslearning?retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
+app.use(fileUpload({
+    createParentPath: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(expressLogger);
 app.use(cors());
 app.use(cookieParser());
 app.use(session({
-  secret: "XjVC2sECqsWCUJYF",
-  name: "langslearning",
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
+    secret: "XjVC2sECqsWCUJYF",
+    name: "langslearning",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 
 app.use(passport.initialize());
@@ -37,19 +41,19 @@ app.use(passport.session());
 require('./auth')(app);
 
 const routers = [
-  require('./class').router,
-  require('./class_template').router,
-  require('./contact').router,
-  require('./order').router,
-  require('./signin').router,
-  require('./static').router,
-  require('./student').router,
-  require('./teacher').router,
-  require('./trial').router,
+    require('./class').router,
+    require('./class_template').router,
+    require('./contact').router,
+    require('./order').router,
+    require('./signin').router,
+    require('./static').router,
+    require('./student').router,
+    require('./teacher').router,
+    require('./trial').router,
 ];
 
 routers.forEach(route => route.apply(app));
 
 app.listen(port, () => {
-  logger.info(`Example app listening at port ${port}`)
+    logger.info(`Example app listening at port ${port}`)
 });
